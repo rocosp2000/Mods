8194a1ba40c735af7d5f9b514b22a567b650d96e
Stelios Pistoftzian <rocosp2000gaming@gmail.com>
2016-05-28 22:50:41 -0400

    sasda

---
+++ @Eden Objects/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 686802825;
+name = "Eden Objects";
+timestamp = 5247685779321498714;
---
+++ @Eden Objects/mod.cpp
@@ -0,0 +1,13 @@
+// Mod Details
+
+picture = "\3den_objects\icons_f\3den_objects_icon_ca.paa";
+logo = "\3den_objects\icons_f\3den_objects_logo_ca.paa";
+logoOver = "\3den_objects\icons_f\3den_objects_logo_hover_ca.paa";
+tooltipOwned = "Eden Objects";
+action="https://forums.bistudio.com/topic/188723-eden-objects/";
+dlcColor[]={0.31,0.77999997,0.77999997,1};
+overview="A selection of objects created to celebrate the launch of the Eden editor.";
+hideName=1;
+hidePicture=0;
+name="Eden Objects";
+dir="3den_Objects";
